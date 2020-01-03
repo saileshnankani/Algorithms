93d51a2945c4610b1138d0636c3b833c4b8c60d2
Sailesh Nankani <saileshhps@gmail.com>
2020-01-03 13:10:22 -0500

    Cleaned up

--- dictionaries/BST.h
+++ dictionaries/BST.h
@@ -11,5 +11,18 @@ class BST{
     
 }
 
+<<<<<<< HEAD
+=======
+        friend std::ostream& operator<<(std::ostream& out, const BST& other){
+            if(other.root){
+                std::cout<<other.root->left<<std::endl;
+                out<<other.root->left<<"("+ std::to_string(other.root->element.first)+","+ std::to_string(other.root->element.second)+") "<<other.root->right;
+            }
+            else{
+                out<<"";
+            }
+        }
+};
+>>>>>>> 154c0ba... Cleaned up
 
 #endif
